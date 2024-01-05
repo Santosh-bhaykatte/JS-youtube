fb3d6ae3ec74bad1fa68154cb3db3f85e5a66b61
Santosh Bhaykatte <125955604+Santosh-bhaykatte@users.noreply.github.com>
2024-01-05 10:44:22 +0000

    dataTypes in JS

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("HEllO");
+
+let isLoggedIn = true;
+let age;
+age = 34.4;
+
+// primitive types
+
+// number => up to 2^53
+// bigInt
+// string => "Santosh"
+// boolean => true/false
+
+// null => standalone value
+// undefined => not known
+// symbol => unique
+
+// object
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
