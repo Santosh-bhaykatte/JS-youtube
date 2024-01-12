33c1fbecbab64766684b18966ee03b16f545822a
Santosh Bhaykatte <125955604+Santosh-bhaykatte@users.noreply.github.com>
2024-01-12 15:58:48 +0000

    objects in JS

---
+++ 02_basics/01_objects.js
@@ -0,0 +1,65 @@
+// Object declaration => key:value pairs
+
+// Always singleton - using constructor
+// Object.create
+
+// Object literals
+
+const mySym = Symbol("key1")
+
+const JsUser = {
+    name: "Hitesh",
+    "full name": "Hitesh Choudhary",
+    // mySym: "mykey1", // Invalid
+    [mySym]: "mykey1",
+    age: 18,
+    location: "jaipur",
+    email: "hitesh@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "saturday"]
+}
+
+// console.log(JsUser.email) // Not right way
+
+// console.log(JsUser["email"])
+
+// console.log(JsUser["full name"])
+
+// ++++++++++++++++++++++++++++++++++++++++++
+
+// console.log(JsUser.mySym)  // wrong way
+// console.log(JsUser[mySym]) // right way
+
+// console.log(typeof("mySym")) // => string
+// console.log(typeof JsUser.mySym)
+
+// ++++++++++++++++++++++++++++++++++++++++++
+
+JsUser.email = "hitesh@chatgpt.com"
+
+// Object.freeze(JsUser) // locks an object
+
+JsUser.email = "hitesh@microsoft.com"
+
+// console.log(JsUser)
+
+// ++++++++++++++++++++++++++++++++++++++++++
+
+// adding function in Object
+
+JsUser.greeting = function(){
+    console.log("Hello JS user")
+}
+
+// console.log(JsUser.greeting()) // function executes
+
+// console.log(JsUser.greeting)  // only reference
+
+// ++++++++++++++++++++++++++++++++++++++++++
+
+JsUser.greeting2 = function(){
+    console.log(`Hello JS user, ${this.name}`) // 'this' keyword used to access current object
+}
+
+console.log(JsUser.greeting())
+console.log(JsUser.greeting2())
---
+++ 02_basics/02_objects.js
@@ -0,0 +1,88 @@
+// Object => singleton using constructor
+
+// Object.freeze(obj) => locks an object from modification
+
+// const tinderUser = {}
+
+const tinderUser = new Object()
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+
+console.log(tinderUser)
+
+// Object.keys(object) => returns array of keys
+console.log(Object.keys(tinderUser))
+
+// Object.values(object) => returns array of values
+console.log(Object.values(tinderUser))
+
+// Object.entries(object) => each key: value pair is converted to array in main array
+console.log(Object.entries(tinderUser))
+
+// returns true OR false
+console.log(tinderUser.hasOwnProperty('isLoggedIn'))
+
+// ++++++++++++++++++++++++++++++++++++++++
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userFullname: {
+            firstname: "Hitesh",
+            lastname: "Choudhary"
+        }
+    }
+}
+
+// console.log(regularUser.fullname)
+
+// console.log(regularUser.fullname.userFullname["lastname"])
+
+// ++++++++++++++++++++++++++++++++++++++++
+
+const obj1 = {
+    1: "a",
+    2: "b"
+}
+
+const obj2 = {
+    3: "a",
+    4: "b"
+}
+
+// const obj3 = {obj1, obj2}
+
+// Object.assign(target, source) 
+// => combines properties of one OR more source objects to target object
+
+// const obj3 = Object.assign({}, obj1, obj2)
+
+// const obj3 = {...obj1, ...obj2} // spread operator
+// console.log(obj3)
+
+// ++++++++++++++++++++++++++++++++++++++++
+
+// Array of objects
+
+let users = [
+    {
+        email: "h@gmail.com",
+        id: 1
+    },
+    {
+        email: "B@gmail.com",
+        id: 2
+    },
+    {
+        email: "K@gmail.com",
+        id: 3
+    },
+]
+
+// console.log(users[1].email)
+
+
+
+
